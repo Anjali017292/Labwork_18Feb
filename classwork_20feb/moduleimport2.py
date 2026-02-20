171b9097008331c4424013ad7da6254d1842fc3f
Anjali017292 <jattianjali1703@gmail.com>
2026-02-20 20:55:04 +0530

    this file contain import module program with output

---
+++ classwork_20feb/moduleimport2.py
@@ -0,0 +1,21 @@
+#to import the module
+from numericcalculation import*
+
+
+#To display the addition of two numbers 
+displayaddition(30,89)
+
+#To display the subtraction of two numbers
+displaysubtraction(80,40)
+
+#To display the multiplication of two numbers
+displayproduct(20,50)
+#To display the division of two numbers
+displayDivision(100,20)
+
+
+
+'''output:addition of 30 and 89 is: 119
+Addition of 80 and 40 is: 40
+Product of 20 and 50 is: 1000
+'''
