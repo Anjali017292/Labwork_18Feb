fc6eb2abb3e14368934390ef70c5082cd7d3ffe6
Anjali017292 <jattianjali1703@gmail.com>
2026-02-20 22:01:14 +0530

    this file contain inverted pyramid program with output

---
+++ pattern_self_learning_20feb/inverted_pyramid.py
@@ -0,0 +1,16 @@
+# Inverted Pyramid Pattern
+rows = 5
+for i in range(rows,0,-1):
+    for j in range(rows-i):
+        print(" ", end="")
+    for k in range(2*i-1):
+        print("*", end="")
+    print()
+    
+'''output:
+*********
+ *******
+  *****
+   ***
+    *
+''' 
