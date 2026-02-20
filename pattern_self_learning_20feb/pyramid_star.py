65f70377e2994e8e1510f0ccf02bda7f79f02dcf
Anjali017292 <jattianjali1703@gmail.com>
2026-02-20 22:05:30 +0530

    this file contain pyramid star program with output

---
+++ pattern_self_learning_20feb/pyramid_star.py
@@ -0,0 +1,15 @@
+# Pyramid Star Pattern
+rows = 5
+for i in range(rows):
+    for j in range(rows-i-1):
+        print(" ", end="")
+    for k in range(2*i+1):
+        print("*", end="")
+    print()
+    
+'''output:
+    *
+   ***
+  *****
+ *********
+'''
